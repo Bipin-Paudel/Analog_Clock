db91b52621447d927fc4bd81bb002ffa949740a8
Bipin Paudel <paudelbipin19@gmail.com>
2025-03-18 12:41:34 +0545

    changes maded

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 setInterval(()=>{
- d = new Date();
-   htime = d.getHours();
-   mtime = d.getMinutes();
-   stime = d.getSeconds();
+   let d = new Date();
+   let htime = d.getHours();
+   let mtime = d.getMinutes();
+   let stime = d.getSeconds();
 
-   hrotation =30*htime/2;
-   mrotation = 6*mtime;
-   srotation = 6*stime;
+   let hrotation =30 * htime + mtime / 2;
+   let mrotation = 6*mtime;
+   let srotation = 6*stime;
 
-   hour.style.transform = `rotate(${hrotation}deg)`;
-   minute.style.transform = `rotate(${mrotation}deg)`;
-   second.style.transform = `rotate(${srotation}deg)`;
+    document.getElementById("hour").style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById("minute").style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById("second").style.transform = `rotate(${srotation}deg)`;
 
 }, 1000)
